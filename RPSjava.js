711fb339a45fe63f49b8e4dc86b06d31b7101572
Chelanp <chelan.pitkin@gmail.com>
2022-01-16 22:35:11 -0700

    I'll get better at messages just  eventually

--- RPSjava.js
+++ RPSjava.js
@@ -1,4 +1,19 @@
 function computerPlay() {
     const choices = ('Rock', 'Paper', 'Scissors')
+    
+    if("Rock".equalsIgnoreCase(choices)) {
+        number = 1;
+    }
+    else if("Paper".equalsIgnoreCase(choices)) {
+        number = 2;
+    }
+    else if("Scissors".equalsIgnoreCase(choices)) {
+        number = 0;
+    }
+    else {
+        number = -1;
+    }
 
+    Math.floor(Math.random() * choices)
 }
+console.log(computerPlay())
